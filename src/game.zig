4323fcb46743aaa83a5cd4af40e63b95ae817e1c
Timothy Cherney <tjcherney@hotmail.com>
2025-04-17 15:00:49 -0700

    basic drawing of world implemented, need fix from renderer to continue

--- src/game.zig
+++ src/game.zig
@@ -11,15 +11,21 @@ pub const Game = struct {
     allocator: std.mem.Allocator = undefined,
     frame_limit: u64 = 16_666_667,
     lock: std.Thread.Mutex = undefined,
+    world: engine.Texture,
+    window: engine.Texture,
     const Self = @This();
     pub const Error = error{} || engine.Error || std.posix.GetRandomError || std.mem.Allocator.Error;
     pub fn init(allocator: std.mem.Allocator) Error!Self {
         return Self{
             .allocator = allocator,
+            .world = engine.Texture.init(allocator),
+            .window = engine.Texture.init(allocator),
         };
     }
     pub fn deinit(self: *Self) Error!void {
         try self.e.deinit();
+        self.world.deinit();
+        self.window.deinit();
     }
 
     pub fn on_mouse_change(self: *Self, mouse_event: engine.MouseEvent) void {
@@ -40,15 +46,23 @@ pub const Game = struct {
     }
 
     pub fn on_render(self: *Self, dt: u64) !void {
-        //self.e.renderer.set_bg(0, 0, 0, self.current_world.tex);
-        _ = self;
+        self.e.renderer.set_bg(0, 0, 0, self.window);
         _ = dt;
-        //try self.e.renderer.flip(self.current_world.tex, self.current_world.viewport);
+        try self.e.renderer.draw_ascii_buffer(self.world.pixel_buffer, self.world.ascii_buffer, self.world.width, self.world.height, common.Rectangle{ .x = 0, .y = 0, .width = self.world.width, .height = self.world.height }, common.Rectangle{ .x = 0, .y = 0, .width = self.world.width, .height = self.world.height }, self.window);
+        GAME_LOG.info("color buffer {any}\n ascii buffer {any}", .{ self.window.pixel_buffer, self.window.ascii_buffer });
+        try self.e.renderer.flip(self.window, null);
     }
     pub fn run(self: *Self) !void {
         self.lock = std.Thread.Mutex{};
         self.e = try Engine(.ascii, .color_true).init(self.allocator);
         GAME_LOG.info("starting height {d}\n", .{self.e.renderer.terminal.size.height});
+        self.world.is_ascii = true;
+        self.window.is_ascii = true;
+        try self.window.rect(@intCast(self.e.renderer.terminal.size.width), @intCast(self.e.renderer.terminal.size.height), 0, 0, 0, 255);
+        try self.world.rect(@intCast(self.e.renderer.terminal.size.width), @intCast(self.e.renderer.terminal.size.height), 255, 0, 0, 255);
+        for (0..self.world.ascii_buffer.len) |i| {
+            self.world.ascii_buffer[i] = '#';
+        }
         self.e.on_key_down(Self, on_key_down, self);
         self.e.on_render(Self, on_render, self);
         self.e.on_mouse_change(Self, on_mouse_change, self);
